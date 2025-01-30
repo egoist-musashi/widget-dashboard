9faca30e82f098c83d319f433d8cc0414b111a14
priyanshu-rathore-slt <priyanshu@sltechnocrats.net>
2025-01-30 16:04:29 +0530

    update changes

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
-import express, {Request, Response} from "express";
-import sequelize from "./src/database"
-import routes from "./src/routes"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import sequelize from "./src/database";
+import routes from "./src/routes";
 const app = express();
 const port = process.env.PORT ?? 3000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
-app.use("/api", routes)
+app.use("/api", routes);
 app.get("/", async (req: Request, res: Response) => {
-    res.send("Hello, project dashboard initialized");
-})
-app.listen(port, async() => {
-    try{
-        await sequelize.authenticate();
-        console.log("Connection to the database has been established successfully.");
-    }catch(error){
-        console.error("Unable to connect to the database: ", error);
-    }
-    console.log(`Server is running at http://localhost:${port}`);
+  res.send("Hello, project dashboard initialized");
+});
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
+  console.log(`Server is running at http://localhost:${port}`);
 });
